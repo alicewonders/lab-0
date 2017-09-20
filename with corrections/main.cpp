baee80e289403850a63ddc0c6276b98f5bcca20d
alicewonders <32062286+alicewonders@users.noreply.github.com>
2017-09-20 16:12:20 +0700

    Update main.cpp

--- with corrections/main.cpp
+++ with corrections/main.cpp
@@ -2,8 +2,11 @@
 #include "module2.h"
 #include <iostream>
 
+//чтобы не прописывать каждый раз std:: ... , необходимо прописать using namespace std; - использование стандартното пр-ва имён
+
 int main(int argc, char** argv)
 {
+	//т.к. стандартное пр-во имён не подключено, то пишем std:: - чтобы показать, что мы используем функцию стандартного вывода
 	std::cout << "Hello world! It's my first cpp project!" << "\n";
 
 	std::cout << Module1::getMyName() << "\n";
@@ -11,15 +14,15 @@ int main(int argc, char** argv)
 
 	using namespace Module1;
 	std::cout << getMyName() << "\n"; // (A) //происходит обращение к пространству имён Module1 и вывод работы его функции getmyName на экран
+	//квадроточие - это оператор определения области видимости
 	std::cout << Module2::getMyName() << "\n";
 
 	//using namespace Module2; // (B)
 	//std::cout << getMyName() << "\n"; // COMPILATION ERROR (C)
 	//ошибка возникает из-за того, что мы уже объявили локальную функцию getMyName(), 
 	//необходимо использовать объявление, как изначально написано строкой ниже:
-
 	using Module2::getMyName;
-	std::cout << getMyName() << "\n"; // (D) //
+	std::cout << getMyName() << "\n"; // (D) //выше указали, что хотим использовать функцию getmyName из Модуля 2, в данной строке происходит использование и вывод её результата
 	//или использовать std::cout << Module2::getMyName()
 	
 	return 0;
